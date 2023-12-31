578d058c35bd987b4c382039eb0fcb6924c5a78a
Eir1ysk4 <135789854+Eir1ysk4@users.noreply.github.com>
2023-12-31 15:56:06 +0000

    sudoku

--- src/naked_triples.h
+++ src/naked_triples.h
@@ -2,4 +2,28 @@
 
 #include "sudoku.h"
 
+typedef struct NakedTriple_impl
+{
+    Cell *p_cell1;
+    Cell *p_cell2;
+    Cell *p_cell3;
+    Cell **p_cells;
+    int value1;
+    int value2;
+    int value3;
+} NakedTriple;
+
+int is_naked_triple_set(Cell **p_cells, int value1, int value2, int value3);
+
+bool is_in_naked_triple_list(NakedTriple *p_array, Cell *p);
+
+int is_naked_triple_cell(Cell *p_cell, int value1, int value2, int value3);
+
+int find_naked_triple_values(Cell **p_cells, int *naked_triple_values);
+
+void find_naked_triple(Cell **p_cells, NakedTriple *p_naked_triple,
+                        int *p_counter);
+
 int naked_triples(SudokuBoard *p_board);
+
+bool is_candidate(Cell*cell, int value);
